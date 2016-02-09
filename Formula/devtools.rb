5ed5d27dd4f2e8956712d9dd2d16a1a44b4f5b52
Frank Febbraro <frank@phase2technology.com>
2016-02-09 11:44:17 -0800

    Added a test

--- Formula/devtools.rb
+++ Formula/devtools.rb
@@ -9,7 +9,6 @@ class Devtools < Formula
   version "0.0.1"
   sha256 "8872afea5c2e23f0f9722ad41b9cb888df0e9f5e2994d25b3362e821922b5830"
 
-  # depends_on "cmake" => :build
   depends_on "docker" => :recommended
   depends_on "docker-compose" => :recommended
   depends_on "docker-machine" => :recommended
@@ -19,15 +18,6 @@ class Devtools < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test devtools`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/devtools", "--version"
   end
 end
