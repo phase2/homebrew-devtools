581fc3ccd9ec5c0332977a2cc31fa29128ba650b
Frank Febbraro <frank@phase2technology.com>
2016-03-11 14:33:43 -0800

    Update for devtools version 0.2.0

--- Formula/devtools.rb
+++ Formula/devtools.rb
@@ -1,17 +1,17 @@
 # Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Devtools < Formula
-  desc "Containerized platform environment for projects"
+  desc "Containerized platform environment for projects. See https://phase2.github.io/devtools for documentation. "
   homepage "https://phase2.github.com/devtools"
-  url "https://s3.amazonaws.com/phase2.devtools/devtools-0.1.3.tar.gz"
-  version "0.1.3"
-  sha256 "a77cac78941b7273919548b150c38a877545332ae7cd78be1e7026b15160ce61"
+  url "https://s3.amazonaws.com/phase2.devtools/devtools-0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "76cc349085d013087dd242a5a0454307d54117a9d826f54fc671c84954a3e021"
 
   def install
     bin.install "devtools"
     bin.install "docker-machine-nfs.sh"
+    bin.install "docker-machine-watch-rsync.sh"
   end
 
   test do
