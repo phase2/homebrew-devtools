87cf2ca7ed7f4ddd3fe4d86b605dd8ba599e4426
Frank Febbraro <frankfebbraro@gmail.com>
2016-04-13 22:40:22 -0700

    version 0.3.0 of devtools

--- Formula/devtools.rb
+++ Formula/devtools.rb
@@ -1,12 +1,9 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-
 class Devtools < Formula
   desc "Containerized platform environment for projects. See https://phase2.github.io/devtools for documentation. "
   homepage "https://phase2.github.com/devtools"
-  url "https://s3.amazonaws.com/phase2.devtools/devtools-0.2.2.tar.gz"
-  version "0.2.2"
-  sha256 "0efad9e3e8aa2bbbc73d931f70b0725c6d2352f0844a4f4928668ec57656716f"
+  url "https://s3.amazonaws.com/phase2.devtools/devtools-0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "ac0576465d50d2c5f7df3ce03eb1f29b63e722645c1f25aa1a3da3a0700c5b34"
 
   def install
     bin.install "devtools"
