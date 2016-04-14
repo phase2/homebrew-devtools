474ec11a4b64d4cc62532f1dedf8918b36639076
Frank Febbraro <frankfebbraro@gmail.com>
2016-04-13 21:58:40 -0700

    updated machine sha256

--- Formula/docker-machine.rb
+++ Formula/docker-machine.rb
@@ -6,7 +6,7 @@ class DockerMachine < Formula
 
   url "https://github.com/docker/machine/releases/download/v0.5.4/docker-machine_darwin-amd64"
   version "0.5.4"
-  sha256 "c08296e94dae10b50e8360e4dfd23c0e35bc30156ccebb3e63c83bd5dc88c4bb"
+  sha256 "48be67863ed8b90bc4ca7a2214587bcc872f4510352dc85a1eb440dac3507389"
 
   def install
     bin.install "docker-machine_darwin-amd64"
