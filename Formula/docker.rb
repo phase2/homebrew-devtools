9bf83ba9f849f24828e554d17d5f506f4341a526
Frank Febbraro <frankfebbraro@gmail.com>
2016-04-13 21:47:23 -0700

    Testing a new install of docker from binary

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -1,32 +1,14 @@
 class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
-  url "https://github.com/docker/docker.git",
-      :tag => "v1.9.1",
-      :revision => "a34a1d598c6096ed8b5ce5219e77d68e5cd85462"
 
-  head "https://github.com/docker/docker.git"
-
-  option "with-experimental", "Enable experimental features"
-  option "without-completions", "Disable bash/zsh completions"
-
-  depends_on "go" => :build
+  url "https://get.docker.com/builds/Darwin/x86_64/docker-1.9.1"
+  version "1.9.1"
+  sha256 "8750ccc2098ec94ef7db110e0016ab02cfa47a1a76f0deb3faa50335b5ec0df9"
 
   def install
-    ENV["AUTO_GOPATH"] = "1"
-    ENV["DOCKER_CLIENTONLY"] = "1"
-    ENV["DOCKER_EXPERIMENTAL"] = "1" if build.with? "experimental"
-
-    system "hack/make.sh", "dynbinary"
-
-    build_version = build.head? ? File.read("VERSION").chomp : version
-    bin.install "bundles/#{build_version}/dynbinary/docker-#{build_version}" => "docker"
-
-    if build.with? "completions"
-      bash_completion.install "contrib/completion/bash/docker"
-      fish_completion.install "contrib/completion/fish/docker.fish"
-      zsh_completion.install "contrib/completion/zsh/_docker"
-    end
+    bin.install "docker-1.9.1"
+    mv bin/"docker-1.9.1", bin/"docker"
   end
 
   test do
