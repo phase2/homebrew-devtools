10dc3eb319400e558cea2bd424f3c065da7c57a2
Frank Febbraro <frankfebbraro@gmail.com>
2016-12-07 14:50:01 -0500

    1.0.4 release

--- Formula/devtools.rb
+++ Formula/devtools.rb
@@ -1,9 +1,9 @@
 class Devtools < Formula
   desc "Containerized platform environment for projects. See https://phase2.github.io/devtools for documentation. "
   homepage "https://phase2.github.com/devtools"
-  url "https://s3.amazonaws.com/phase2.devtools/devtools-1.0.3.tar.gz"
-  version "1.0.3"
-  sha256 "5d9d9f9e910d7c559400ce70d26104f0c00d32ab7ee8aee2e0dd7e5bdb1da9b8"
+  url "https://s3.amazonaws.com/phase2.devtools/devtools-1.0.4.tar.gz"
+  version "1.0.4"
+  sha256 "332f7a9bd582fe20f89c5f073f78a792b27b455cfca3b1329ad6c4b8c70ae8f0"
 
   depends_on "docker"
   depends_on "docker-machine"
